b8faab13ceef04aaf5b7993c572504f8a2e57dab
Sandra <hi-sandra@hotmail.com>
2026-06-15 19:01:02 -0600

    posición del regalo

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function findGift(gifts, giftName, index = 0) {
      if (index >= gifts.length) {
         return "El regalo no está en la lista";
     }
+
+     if (gifts[index] === giftName) {
+        return "El regalo está en la posición: " + index;
+    }
